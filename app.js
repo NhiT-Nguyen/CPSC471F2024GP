e9156b438f6837fd28f7abf6a83846f71f96f52c
NhiT-Nguyen <122294591+NhiT-Nguyen@users.noreply.github.com>
2024-12-04 17:02:00 -0700

    merge

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const db = require('./backend/config/db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const blogRoutes = require('./backend/routes/blogRoutes');
 
 app.use(express.json());
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}   );
+
 app.use('/blog', blogRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 }
 );
+
+app.get('/blog', (req, res) => {
+  res.send('this is the blog page');
+
+})
+
 module.exports = app;
